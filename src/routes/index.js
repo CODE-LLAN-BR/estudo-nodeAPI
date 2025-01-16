30149d887862958baf0aa0a7e8ef0bc29262ad9e
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-16 11:34:58 -0300

    first commit for page and api integration / created sessionsController and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
-const {Router} = require('express');
+const { Router } = require('express');
 
 const usersRoutes = require('./user.routes');
-
 const notesRoutes = require('./notes.routes')
+const tagsRoutes = require("./tags.routes");
+const sessionsRoutes = require("./sessions.routes");
 
-const tagsRoutes = require("./tags.routes")
 
 const routes = Router();
 
 routes.use("/users",usersRoutes);
 routes.use("/notes",notesRoutes)
-routes.use("/tags",tagsRoutes)
+routes.use("/tags",tagsRoutes);
+routes.use("/sessions",sessionsRoutes);
 
 module.exports = routes;
