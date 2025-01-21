8df934f67e143f213067d8054094fc09ec8a33ef
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-21 10:53:07 -0300

    added email verification

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,9 +1,22 @@
+const knex = require("../database/knex");
+
+const AppError = require("../utils/AppError");
+
+
 class SessionsController {
     async create(request, response) {
         const { email , password } = request.body
 
+        const user = await knex('users').where({email}).first();
 
-        return response.json({email, password});
+        if(!user){
+            throw new AppError("E-mail e/ou senha incorreto(a)",401);
+        }
+
+
+
+
+        return response.json(user);
     }
 };
 
