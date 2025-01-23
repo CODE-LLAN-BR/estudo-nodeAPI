8cae35d425a3c1a991b435190cc5ff8c44697979
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-23 10:24:23 -0300

    introducing the token in requests

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const{ Router } = require ("express");
 
 const UsersController = require('../controllers/UsersController');
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
@@ -12,6 +13,6 @@ const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/",ensureAuthenticated, usersController.update);
 
 module.exports = usersRoutes;
