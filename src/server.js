b54a529b1308580f11242fa331282f5c4268354b
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-28 13:18:53 -0300

    installed cors library / imported cors to server and declarated

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors')
 
 const AppError = require('./utils/AppError');
 const migrationsRun = require('./database/sqlite/migrations')
 const uploadConfig = require('./configs/upload')
-
-
 const routes = require('./routes');
 
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
