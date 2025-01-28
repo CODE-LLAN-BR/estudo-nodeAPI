5b4de34184635a296418593e606e5a70a5d7fd24
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-28 10:13:38 -0300

    updated avatar and conections of DB

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 require('express-async-errors');
-const AppError = require('./utils/AppError');
-
-const migrationsRun = require('./database/sqlite/migrations')
-
 const express = require('express');
+
+const AppError = require('./utils/AppError');
+const migrationsRun = require('./database/sqlite/migrations')
+const uploadConfig = require('./configs/upload')
+
 
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes);
 
 migrationsRun();
